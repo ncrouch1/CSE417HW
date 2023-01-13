06373cd6cedc2c7be0dddd5916744f62bc4ebdde
noah <ncrouch1@uw.edu>
2023-01-13 11:09:37 -0800

    Finished HW1, Added Statistics generator for the goodness in the random preference list generated trial.

--- src/HW1/HW1.java
+++ src/HW1/HW1.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 public class HW1 {
     public static void main(String[] args) {
-        int length = 10;
+        int length = 1000;
         int[][] mpref = getRandomPrefs(length);
         boolean[][] asked = new boolean[mpref.length][mpref.length];
         int[][] wpref = getRandomPrefs(length);
@@ -61,7 +61,8 @@ public class HW1 {
             }
         }
         System.out.println(mMatch[length- 1] + "]");
-        printPrefLists(mpref, wpref);
+//        printPrefLists(mpref, wpref);
+        printGoodnessComparison(mMatch, mpref, wpref);
     }
 
     public static int[] setMatches(int l) {
@@ -74,12 +75,13 @@ public class HW1 {
 
     public static int[][] getRandomPrefs(int l) {
         int[][] res = new int[l][l];
-        int[] layer = new int[l];
         for (int i = 0; i < l; i++) {
-            layer[i] = i;
+            for (int j = 0; j < l; j++) {
+                res[i][j] = j;
+            }
         }
-        for (int i = 0; i < l; i++) {
-            res[i] = randomize(layer, l);
+        for (int row = 0; row < l; row++) {
+            res[row] = randomize(res[row], l);
         }
         return res;
     }
@@ -116,5 +118,50 @@ public class HW1 {
             }
             System.out.println(w[i][w[i].length-1] + "]");
         }
+    }
+
+    public static void printGoodnessComparison(int[] mMatches, int[][] mpref, int[][] wpref) {
+        int[] mGoodnesses = new int[mMatches.length];
+        int[] wGoodnesses = new int[mMatches.length];
+        double mMean = 0.0;
+        double wMean = 0.0;
+        for (int index = 0; index < mMatches.length; index++) {
+            int man = index;
+            int woman = mMatches[index];
+            System.out.printf("Testing goodness for match [%d, %d]\n", man, woman);
+            for (int col = 0; col < mpref[man].length; col++) {
+                if (mpref[man][col] == woman) {
+                    mGoodnesses[man] = col;
+                    mMean += col;
+                    break;
+                }
+            }
+            for (int col = 0; col < wpref[woman].length; col++) {
+                if (wpref[woman][col] == man) {
+                    wGoodnesses[woman] = col;
+                    wMean += col;
+                    break;
+                }
+            }
+            System.out.printf("Man goodness = %d, Woman goodness = %d\n", mGoodnesses[man], wGoodnesses[woman]);
+        }
+        mMean = mMean / mGoodnesses.length;
+        wMean = wMean / wGoodnesses.length;
+        Arrays.sort(mGoodnesses);
+        Arrays.sort(wGoodnesses);
+        double mMedian = 0.0;
+        double wMedian = 0.0;
+        if (mGoodnesses.length % 2 == 0) {
+            mMedian = (mGoodnesses[(mGoodnesses.length/2)] + mGoodnesses[(mGoodnesses.length/2) + 1]) / 2;
+            wMedian = (wGoodnesses[(wGoodnesses.length/2)] + wGoodnesses[(wGoodnesses.length/2) + 1]) / 2;
+
+        } else {
+            int i = (mGoodnesses.length / 2) + 1;
+            mMedian = mGoodnesses[i];
+            wMedian = wGoodnesses[i];
+        }
+        System.out.printf("Average statistics, lower is better\n");
+        System.out.printf("Male Goodnesses: Median = %.2f, Mean = %.2f\n", mMedian, mMean);
+        System.out.printf("Female Goodnesses: Median = %.2f, Mean = %.2f\n", wMedian, wMean);
     }
 }
